57fc1e3a4a27d159cbc8cc24625fde1b270028b7
nvdinh185 <nvdinh0766777123@gmail.com>
2022-05-17 10:32:52 +0700

    review code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Compnonent from './components/comment';
+import Comment from './components/comment';
 import Clock from './state/clock';
 
 export default function App() {
     return (
         <Router>
             <Routes>
-                <Route exact path="/" element={<Compnonent />} />
-                <Route path="/:id" element={<Clock />} />
+                <Route exact path="/" element={<Comment />} />
+                <Route path="/clock" element={<Clock />} />
             </Routes>
         </Router>
     )
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 // import App from './event/actionLink';
 // import App from './thinking-react/xuly3';
 // import App from './form/reservation';
-import App from './conditional/loginControl';
+// import App from './conditional/loginControl';
 
 ReactDOM.render(<App />, document.getElementById('root'));
