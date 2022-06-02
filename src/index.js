f5e1836ea49150141e1c5b4f6fe565bdfb95e68d
nvdinh185 <nvdinh0766777123@gmail.com>
2022-06-02 08:15:51 +0700

    add lib new

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import ReactDOM from 'react-dom';
 // import App from './App2';
 // import App from './intro/App';
 import Example from './overview/Example';
+// import App from './App';
 
 ReactDOM.render(<Example />, document.getElementById('root'));
