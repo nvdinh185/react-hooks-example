f41e44845c350cc7ca9427ca7d1452a7ebfa9558
nvdinh185 <nvdinh0766777123@gmail.com>
2022-11-11 14:28:43 +0700

    x

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
-// import App from './App2';
+import App from './App2';
 // import App from './intro/App';
-import Example from './overview/Example';
+// import Example from './overview/Example';
 // import App from './App';
 
-ReactDOM.render(<Example />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
