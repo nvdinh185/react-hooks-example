835fee3fcac294261d422c873d2e8f15a2d2b131
nvdinh185 <nvdinh0766777123@gmail.com>
2022-11-11 14:41:07 +0700

    x

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 // import App from './event/actionLink2';
 // import App from './thinking-react/xuly3';
 // import App from './form/reservation';
 // import App from './conditional/loginControl';
-import App from './composition/xuly4';
+// import App from './composition/xuly4';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
